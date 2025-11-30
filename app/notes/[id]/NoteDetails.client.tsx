8af5ad03df07c43c37f5228cc5835398a1958496
Vasyl Hrubyi <ronaldomen7@gmail.com>
2025-11-30 19:56:49 +0200

    delete-empty-page

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -4,7 +4,7 @@ import { useParams } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 
-import css from "./NoteDetailsPage.module.css";
+import css from "@/components/NotesPage/NotesPage.module.css";
 
 export default function NoteDetailsClient() {
   const { id } = useParams();
